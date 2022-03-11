c7fa0fd056efb1d65f0cf9a4520f0c21d08ae2c0
realninety5 <kingsleyog95@gmail.com>
2022-03-11 17:03:58 +0100

    Restrusturing the Dashbox

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <template>
   <div id="app">
-    <!-- <div id="nav">
+    <div id="nav">
      <router-link to="/">Home</router-link>
       <router-link to="/about">About</router-link>
-    </div> -->
+      <router-link to="/LandingPage">Landing Page</router-link>
+      <router-link to="/Dashboard">Dashboard</router-link>
+      </div>
     <router-view/>
   </div>
 </template>
--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,26 +1,11 @@
 <template>
     <div id="grid-container">
-        <div class="top-bar">
-            <div class="logo">
-                <!-- <img class="bfreelogo" src="../assets/bfreeLogo.jpg" alt="fidbak logo"><img src="../assets/FIDBAK.jpg" alt=""> -->
-                <img src="../assets/logo.jpg" alt="fidbak logo">
-            </div>
-            <div class="top-nav">
-                    <h3>Hello Bolade!</h3>
-                    <div class="icons">
-                        <!-- placeholder for icons -->
-                        <p><font-awesome-icon :icon="['fas', 'search']"/></p>
-                        <p><font-awesome-icon :icon="['fas', 'bell']"/></p>
-                        <p><font-awesome-icon :icon="['fas', 'user-circle']"/></p>
-                        <!-- replace line below with appropriate icon -->
-                        <p><font-awesome-icon :icon="['fas', 'tools']"/></p>
-                    </div>
-            </div>
-            </div>
-            <!-- The body starts here and should have two grids -->
-        <div class="body-container">
-            <nav class="nav-bar">
-                <div>
+            <!-- this is the side nav bar container -->
+            <div class="side-nav">
+                <div class="logo">
+                    <img src="../assets/bfreeLogo.jpg" alt="bfree logo"><img src="../assets/fidbak.jpg" alt="fidbak log">
+                </div>
+                <div class="nav">
                 <p class="home-icon"><font-awesome-icon :icon="['fas', 'house']"/> Home</p>
                 <p class="bell-icons"><font-awesome-icon :icon="['fas', 'comments']"/>Feedback</p>
                 <p class="bell-icons"><font-awesome-icon :icon="['fas', 'bell']"/> Notification</p>
@@ -29,32 +14,46 @@
                 <div>
                     <p class="log-out-icon"><font-awesome-icon :icon="['fas', 'sign-out']"/>Log Out</p>
                 </div>
-            </nav>
-            <div class="main-dashboard">
-                <div class="user-details">
-                    <h5 class="staff-id">Staff id<br> BFR00055</h5>
-                    <h5>Account Manager<br> Branch</h5>
-                    <h5>Email<br> bolade@bfree.limited</h5>
+            </div>
+        <div class="main-body">
+            <div class="top-nav">
+                <h3 class="hello-Bolade">Hello Bolade!</h3>
+                <div class="icons">
+                        <!-- placeholder for icons -->
+                    <p><font-awesome-icon :icon="['fas', 'search']"/></p>
+                    <p><font-awesome-icon :icon="['fas', 'bell']"/></p>
+                    <p><font-awesome-icon :icon="['fas', 'user-circle']"/></p>
+                        <!-- replace line below with appropriate icon -->
+                    <p><font-awesome-icon :icon="['fas', 'tools']"/></p>
                 </div>
-                <div class="feedback-container">
-                    <div class="feedbacks-box">
-                        <h4>Feedbacks</h4>
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea amet officia ullam. Adipisci magni atque, fugiat, aut similique dolor iste eum sint </p>
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea amet officia ullam. Adipisci magni atque, fugiat, aut similique dolor iste eum sint </p>
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea amet officia ullam. Adipisci magni atque, fugiat, aut similique dolor iste eum sint </p>
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea amet officia ullam. Adipisci magni atque, fugiat, aut similique dolor iste eum sint </p>
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea amet officia ullam. Adipisci magni atque, fugiat, aut similique dolor iste eum sint </p>
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea amet officia ullam. Adipisci magni atque, fugiat, aut similique dolor iste eum sint </p>
-                    </div>
-                    <div class="notifications">
-                        <h4>Notifications</h4>
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea amet officia ullam. Adipisci magni atque, fugiat, aut similique dolor iste eum sint </p>
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea amet officia ullam. Adipisci magni atque, fugiat, aut similique dolor iste eum sint </p>
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea amet officia ullam. Adipisci magni atque, fugiat, aut similique dolor iste eum sint </p>
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea amet officia ullam. Adipisci magni atque, fugiat, aut similique dolor iste eum sint </p>
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea amet officia ullam. Adipisci magni atque, fugiat, aut similique dolor iste eum sint </p>
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea amet officia ullam. Adipisci magni atque, fugiat, aut similique dolor iste eum sint </p>
-                    </div>
+            </div>
+            <div class="user-details">
+                <h5 class="staff-id">Staff id<br> <strong class="bfr">BFR00055</strong></h5>
+                <h5 class="am">Account Manager<br> <strong class="portfolio">Branch</strong></h5>
+                <h5 class="email">Email<br> <strong class="bfr-mail">bolade@bfree.limited</strong></h5>
+            </div>
+        </div>
+        <div>
+            <div class="feedbacks-box">
+                <h4 class="feedback-header">Feedbacks</h4>
+                <div class="feedback-text">
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea amet officia ullam. Adipisci magni atque, fugiat, aut similique dolor iste eum sint </p>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea amet officia ullam. Adipisci magni atque, fugiat, aut similique dolor iste eum sint </p>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea amet officia ullam. Adipisci magni atque, fugiat, aut similique dolor iste eum sint </p>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea amet officia ullam. Adipisci magni atque, fugiat, aut similique dolor iste eum sint </p>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea amet officia ullam. Adipisci magni atque, fugiat, aut similique dolor iste eum sint </p>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea amet officia ullam. Adipisci magni atque, fugiat, aut similique dolor iste eum sint </p>
+                </div>
+            </div>
+            <div class="notifications">
+                <h4 class="feedback-header">Notifications</h4>
+                <div class="feedback-text">
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea amet officia ullam. Adipisci magni atque, fugiat, aut similique dolor iste eum sint </p>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea amet officia ullam. Adipisci magni atque, fugiat, aut similique dolor iste eum sint </p>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea amet officia ullam. Adipisci magni atque, fugiat, aut similique dolor iste eum sint </p>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea amet officia ullam. Adipisci magni atque, fugiat, aut similique dolor iste eum sint </p>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea amet officia ullam. Adipisci magni atque, fugiat, aut similique dolor iste eum sint </p>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea amet officia ullam. Adipisci magni atque, fugiat, aut similique dolor iste eum sint </p>
                 </div>
             </div>
         </div>
@@ -63,78 +62,181 @@
 
 <style scoped>
 #grid-container{
+    position: relative;
+    width: 1440px;
+    height: 1024px;
+    background: linear-gradient(0deg, #F7F7F7, #F7F7F7), rgba(0, 164, 219, 0.32);
+}
+
+.side-nav{
     display: grid;
     grid-template-rows: 1fr 5fr;
-    height: 100vh;
-    grid-gap: none;
-    position: relative;
+    position: absolute;
+    width: 232px;
+    height: 1024px;
+    left: 0px;
+    top: 0px;
+    background: #E3F5FB;
 }
 
-.top-bar{
+/* .nav{
     display: grid;
-    grid-template-columns: 1fr 5fr;
-    width: 95vw;
-    height: 15.789vh;
-    /* box-shadow: 1px 5px 5px rgba(0, 0, 0, 0.25); */
-}
+    grid-template-rows: 1fr 2fr 2fr 2fr 2fr;
+    grid-gap: 50px;
+} */
 
 .logo{
-    /* background-image: url(bfreelogo.jpg); */
-    height: 15.789vh;
-    background-color: #E3F5FB;
-    justify-content: center;
-    text-align: start;
-    width: 14.1vw;
-    line-height: 15.789vh;
-    /* margin-left: 20px; */
+    /* background-image: url("../assets/FIDBAK.jpg"); */
+    position: relative;
+    width: 150px;
+    height: 32px;
+    left: 24px;
+    top: 34px;
 }
 
-.bfreelogo{
-    margin-left: 20px;
-    /* color: #E3F5FB; */
-}
+/* .main-body{
+
+} */
 
 .top-nav{
-    display: grid;
-    grid-template-columns: 2fr 5fr;
-    background-color: #F7F7F7;
-    justify-content: center;
-    align-items: center;
-    grid-gap: 50%;
-    width: 83.58098068350668vw;
+    position: absolute;
+    width: 1208px;
+    margin-left: 232px;
+    /* width: 1440px; */
+    height: 99px;
+    left: 0px;
+    top: 0px;
+    background: #F7F7F7;
+    box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
 }
 
-h3{
-    margin-left: 35px;
+.am
+
+.hello-Bolade{
+    position: absolute;
+    width: 149px;
+    height: 33px;
+    left: 75px;
+    top: 34px;
+    font-family: 'Poppins';
+    font-style: normal;
+    font-weight: 700;
+    font-size: 22px;
+    line-height: 33px;
+    /* identical to box height */
+    color: #000000;
+
 }
 
 .icons{
+    position: absolute;
+    width: 300px;
+    height: 63px;
+    left: 800px;
+    top: 20px;
+    margin-right: 232px;
+    background: rgba(0, 0, 0, 0.08);
+    border-radius: 5px;
     display: grid;
     grid-template-columns: 1fr 1fr 1fr 1fr;
-    width: 23.243647234678626vw;
-    height: 10.047846889952153vh;
-    background: rgba(0, 0, 0, 0.08);
-    left: 1088px;
-    top: 20px;
-    /* position: absolute; */
-    /* gap: 5px; */
 }
 
-.body-container{
-    display: grid;
-    grid-template-columns: 1fr 5fr;
-    height: 147.52vh;
-    width: 100%;
-    /* position: fixed; */
-
+.user-details{
+    display: flex;
+    flex-direction: row;
+    align-items: flex-start;
+    padding: 0px;
+    position: absolute;
+    width: 1123px;
+    height: 68px;
+    left: 273px;
+    top: 131px
 }
 
-.nav-bar{
-    display: grid;
-    grid-template-rows: 5fr 1fr;
-    height: 147.52vh;
-    width: 14.1vw;
-    background-color: #E3F5FB;
+.staff-id{
+    position: static;
+    width: 74px;
+    height: 30px;
+    left: 0px;
+    top: 0px;
+    font-family: 'Poppins';
+    font-style: normal;
+    font-weight: 700;
+    font-size: 20px;
+    line-height: 30px;
+    color: #000000;
+    /* Inside auto layout */
+    flex: none;
+    order: 0;
+    flex-grow: 0;
+    margin: 2px 0px;
+}
+
+.email{
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+    padding: 0px;
+    position: static;
+    width: 217px;
+    height: 60px;
+    left: 898px;
+    top: 0px;
+/* Inside auto layout */
+    flex: none;
+    order: 2;
+    flex-grow: 0;
+    margin: 0px 296px;
+}
+
+/* .bfr{
+    position: absolute;
+    width: 98px;
+    height: 30px;
+    left: 0px;
+    top: 0px;
+    font-family: 'Poppins';
+    font-style: normal;
+    font-weight: 400;
+    font-size: 20px;
+    line-height: 30px; */
+    /* identical to box height */
+    /* color: #000000;
+} */
+
+/* .am{} */
+
+.feedbacks-box{
+    position: absolute;
+    width: 1125px;
+    height: 350px;
+    left: 273px;
+    top: 227px;
+    background: #FFFFFF;
+    box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+}
+
+.notifications{
+    position: absolute;
+    width: 1125px;
+    height: 333px;
+    left: 274px;
+    top: 607px;
+    border-end-end-radius: 0px;
+    background: #F7F7F7;
+    box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+}
+
+.feedback-header{
+        position: absolute;
+        width: 1125px;
+        height: 55px;
+        left: 0px;
+        top: 0px;
+        line-height: 55px;
+        text-align: start;
+        color: #F7F7F7;
+        background: #00A4DB;
 }
 
 .home-icon{
@@ -154,7 +256,6 @@ h3{
     grid-template-columns: 1fr 1fr 1fr 1fr;
     height: 8.77vh;
     position: relative;
-    /* background-color: #00A4DB; */
     justify-content: center;
     align-content: center;
     padding-left: 20px;
@@ -182,86 +283,11 @@ h3{
     /* grid-gap: none; */
 }
 
-.user-details{
-    display: grid;
-    grid-template-columns: 1fr 1fr 1fr;
-    height: 12vh;
-    width: 83.58098068350668vw;
-    text-align: start;
-    /* justify-content:stretch; */
-    /* padding: 0%; */
-    /* background-color: beige; */
-}
-
 .staff-id{
     margin-left: 35px;
 }
 
-.feedback-container{
-    display: grid;
-    grid-template-rows: 1fr 1fr;
-    height: 100%;
-    width: 90vmax;
-    /* position: absolute; */
-}
-
-.feedbacks-box{
-    /* display: flex; */
-    /* position: relative; */
-    width: 75vmax;
-    /* height: 350px; */
-    height: 58vh;
-    /* left: 0px;
-    top: 0px; */
-    margin-left: 20px;
-    margin-right: 20px;
-    text-align: start;
-    text-indent: 20px;
-    line-height: 21px;
-    border-radius: 2px;
-    padding: 5px;
-    background: #FFFFFF;
-    justify-content: center;
-    box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
-    /* margin-top: 2px;
-    width: 100%; */
-}
-
-.notifications{
-    /* display: grid; */
-    position: relative;
-    width: 75vmax;
-    /* height: 350px; */
-    /* height: 58.62646566164154vh; */
-    /* left: 0px; */
-    height: 58vh;
-    /* top: 0px; */
-    margin-left: 20px;
-    margin-right: 20px;
-    text-align: start;
-    text-indent: 20px;
-    line-height: 2.8vh;
-    background-color: #FFFFFF;
-    justify-content: center;
-    box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
-}
-
-h4{
-    height: 8.77vh;
-    /* /* width: 84.45vw; */
-    left: 0px;
-    top: 0px;
-    background-color: #00A4DB;
-    text-align: start;
-    justify-content: center;
-    /* margin-left: 20px;
-    margin-right: 20px; */
-    text-indent: 20px;
-    line-height: 8.77vh;
-}
-
 p:hover{
-    text-decoration: underline #00A4DB 5px;
-    /* color: ; */
+    text-decoration: 5px underline #00A4DB;
 }
 </style>
