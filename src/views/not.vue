9bc62e61d2d9adf679e21701cdd0cf83e7fe3b5c
Wuraola1 <wurafullstack@gmail.comgit config --global user.email wurafullstack@gmail.com>
2022-04-01 04:23:51 +0100

    I just added the components, review the Navbar.vue

---
+++ src/views/not.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="home">
+  <Header/>
+<Navbar/>
+
+  </div>
+</template>
+
+<script>
+import Navbar from '@/components/navbar/Navbar.vue'
+import Header from '@/components/navbar/Header.vue'
+
+export default {
+  name: 'HomeView',
+
+  components: {
+    Navbar,
+    Header
+  }
+
+}
+
+</script>
+
+<style>
+
+</style>
