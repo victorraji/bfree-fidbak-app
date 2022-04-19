f4eb1df9b7d9d6b5cb961480ad5ec72d54defb28
Franssy <onyefrancis98@gmail.com>
2022-04-19 23:45:57 +0100

    Netlify Fix

--- src/components/responder/HeaderR.vue
+++ src/components/responder/HeaderR.vue
@@ -1,5 +1,4 @@
 <template>
-<<<<<<< HEAD
   <header>
     <div class="headerBlock">
       <div></div>
@@ -22,26 +21,6 @@
       </div>
     </div>
   </header>
-=======
-<header>
-<div class="headerBlock">
-<div class="headerI"><h1>{{welcome}}</h1></div>
-<div class="headerIcons">
-<div class="searchIcon">
-<font-awesome-icon :icon="['fas', 'search']"/></div>
-<div class="notification68I" data-number="4">
-<font-awesome-icon :icon="['fas', 'fa-bell']"/></div>
-<div class="userCircle">
-<font-awesome-icon :icon="['fas', 'user-circle']"/>
-</div>
-<div class="cogIcon">
-<font-awesome-icon :icon="['fas', 'fa-cog']"/>
-</div>
-</div>
-</div>
-</header>
-
->>>>>>> main
 </template>
 
 <script>
@@ -56,7 +35,6 @@ export default {
 </script>
 
 <style scoped>
-<<<<<<< HEAD
 header{
   position: sticky;
   top: 0;
@@ -67,21 +45,12 @@ display: flex;
 justify-content: space-between;
 position: fixed;
 mask-position: fixed;
-=======
-.headerBlock {
-display: flex;
-justify-content: space-between;
->>>>>>> main
 background: #F7F7F7;
 box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
 height: 99px;
 align-items: center;
-<<<<<<< HEAD
 margin: 0;
 width: 100%;
-=======
-margin: -70px -30px;
->>>>>>> main
 }
 .headerI {
 color: #000000;
@@ -92,12 +61,8 @@ margin-left: -4%;
 }
 .headerIcons {
 display: flex;
-<<<<<<< HEAD
 /* grid-template-columns: 1fr 1fr 1fr 1fr; */
 justify-content: space-around;
-=======
-justify-content: space-between;
->>>>>>> main
 align-items: center;
 background: rgba(0, 0, 0, 0.08);
 border-radius: 5px;
@@ -105,15 +70,6 @@ height: 63px;
 width: 311px;
 margin: 30px;
 }
-<<<<<<< HEAD
-=======
-.searchIcon {
-margin-left: 30px;
-}
-.cogIcon {
-margin-right: 30px;
-}
->>>>>>> main
 .notification68I {
 position: relative;
 margin-bottom: 10%;
